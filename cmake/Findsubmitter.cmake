81a35bec28e75ddcdd9f571f53d4e441ae368769
Patrick Hopf <patrick.hopf@tum.de>
2024-07-01 11:59:58 +0000

    fetchcontent onnxruntime

--- cmake/Findsubmitter.cmake
+++ cmake/Findsubmitter.cmake
@@ -2,9 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     submitter
-    #GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/submitter.git
-    #GIT_TAG patrick/update
-    SOURCE_DIR /home/ubuntu/mqss/submitter
+    GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/submitter.git
+    GIT_TAG patrick/update
 )
 
 FetchContent_MakeAvailable(submitter)
