33313161657568b26e88a9ab1889412b56158f97
flowerthrower <flowerthrower@users.noreply.github.com>
2024-09-26 16:16:51 +0200

    empty init file

--- predictor/src/predictor/__init__.py
+++ predictor/src/predictor/__init__.py
@@ -1,7 +1,1 @@
 """File to initialize the predictor package."""
-
-from __future__ import annotations
-
-from predictor import data_preparation, training
-
-__all__ = ["data_preparation", "training"]
